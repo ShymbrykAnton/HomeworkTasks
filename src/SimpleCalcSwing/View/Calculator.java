7cc2e7a13dd9fe3320ec8a241477bab5e9659616
Shymbryk <shymbryk@gmail.com>
2020-12-27 20:45:18 +0200

    SimpleCalcSwing CalculatorView minor update

--- src/SimpleCalcSwing/View/Calculator.java
+++ src/SimpleCalcSwing/View/Calculator.java
@@ -11,16 +11,13 @@ import java.awt.event.ActionListener;
 public class Calculator {
 
     public Calculator() {
-//TODO переделать хелп информейшн
         JFrame frame = new JFrame("Калькулятор");
-        JLabel helpInformation = new JLabel("  Данный калькулятор умеет выполнять следующие операции: ");
-        JLabel helpInformation1 = new JLabel("суммирование  -  \"+\", вычитание  -  \"-\", умножение  -  \"*\",");
-        JLabel helpInformation2 = new JLabel("деление - \"/\", вычисление корня число 2-степени - \"root\",");
-        JLabel helpInformation3 = new JLabel("возведение числа в число 2-степень - \"-pow\".");
-        helpInformation.setBounds(10, 20, 380, 40);
-        helpInformation1.setBounds(10, 40, 380, 40);
-        helpInformation2.setBounds(10, 60, 380, 40);
-        helpInformation3.setBounds(10, 80, 380, 40);
+        JTextArea helpTextArea = new JTextArea();
+        helpTextArea.setBounds(10,20,360,80);
+        helpTextArea.setText("    Данный калькулятор умеет выполнять следующие операции: " +
+                "\nсуммирование   -  \"+\", вычитание   -  \"-\", умножение  -  \"*\"," +
+                "\nделение - \"/\", вычисление корня число 2-степени - \"root\"," +
+                "\nвозведение числа в число 2-степень - \"-pow\".");
 
         JLabel labelNumber1 = new JLabel("Число 1");
         JTextField textFieldNumber1 = new JTextField();
@@ -43,11 +40,7 @@ public class Calculator {
         labelResult.setBounds(45, 330, 100, 40);
         textAreaResult.setBounds(150, 330, 190, 40);
 
-
-        frame.add(helpInformation);
-        frame.add(helpInformation1);
-        frame.add(helpInformation2);
-        frame.add(helpInformation3);
+        frame.add(helpTextArea);
         frame.add(labelNumber1);
         frame.add(textFieldNumber1);
         frame.add(labelNumber2);
